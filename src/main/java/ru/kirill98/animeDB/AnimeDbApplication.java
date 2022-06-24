17c2f518be4c5622a61af298d54ef1d70d257c1e
Lyubarskikh Kirill <k.lubarskih@mail.ru>
2022-06-24 10:53:54 +0300

    Init commit

---
+++ src/main/java/ru/kirill98/animeDB/AnimeDbApplication.java
@@ -0,0 +1,16 @@
+package ru.kirill98.animeDB;
+
+import lombok.extern.log4j.Log4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Log4j
+@SpringBootApplication
+public class AnimeDbApplication {
+
+	public static void main(String[] args) {
+		log.info("Was start app");
+		SpringApplication.run(AnimeDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/kirill98/animeDB/AnimeDbApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kirill98.animeDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
